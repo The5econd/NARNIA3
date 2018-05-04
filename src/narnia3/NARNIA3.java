621b9c45827bd6c0b809da859beeb3cbcbf8070e
Fer Salazar <00017917@uca.edu.sv>
2018-05-03 20:42:44 -0600

    se agrego la clase main al proyecto

---
+++ src/narnia3/NARNIA3.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package narnia3;
+
+/**
+ *
+ * @author thesecond
+ */
+public class NARNIA3 {
+
+    public static void main(String[] args) {
+        AdminHotel Admin = new AdminHotel();
+    }
+    
+}
