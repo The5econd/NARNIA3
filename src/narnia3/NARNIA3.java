ab7f3d099b29cb4405a71532ace4cd77b2e87cdb
Fer Salazar <00017917@uca.edu.sv>
2018-05-03 23:20:59 -0600

    se empezo a realizar el metodo menu en la clase main

--- src/narnia3/NARNIA3.java
+++ src/narnia3/NARNIA3.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package narnia3;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 
 /**
  *
@@ -12,7 +15,47 @@ package narnia3;
 public class NARNIA3 {
 
     public static void main(String[] args) {
+        AdminHotel ADMIN = new AdminHotel();
+    }
+    public void menu(){
         AdminHotel Admin = new AdminHotel();
+        Cliente CLIENTE = new Cliente();
+        String opcion;
+        Scanner input= new Scanner(System.in);
+        System.out.println("***************MENU***************");
+        System.out.println("1.Ver reservaciones de la semana");
+        System.out.println("2.Habilitar habitaciones");
+        System.out.println("3.Habilitar piso");
+        System.out.println("4.Modificar habitacion");
+        System.out.println("5.Agregar cliente");
+        System.out.println("6.Mostrar habitaciones disponibles");
+        System.out.println("7.Hacer reservacion");
+        System.out.println("8.Cancelar reservacion");
+        System.out.println("9.MOdificar reservacion");
+        opcion=input.next();
+        int OP=Integer.parseInt(opcion);
+        switch(OP){
+            case 1:
+            case 2:
+                Admin.HabilitarHabitacion();
+                break;
+            case 3:
+                Admin.HabilitarPiso();
+                break;
+            case 4:
+                Admin.ModificarHabitacion();
+                break;
+            case 5:
+            case 6:
+                Admin.MostrarHabitacionesDisponibles();
+                break;
+            case 7:
+                CLIENTE.HacerReservacion();
+                break;
+            case 8:
+            case 9:
+             
+            }
     }
     
 }
